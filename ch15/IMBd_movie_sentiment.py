5a64343fc6cc12ae32dd9e53bdf4bfefe7961097
driessenslucas <lucas.driessens@hotmail.com>
2024-11-11 19:41:18 +0100

    ch15

--- ch15/IMBd_movie_sentiment.py
+++ ch15/IMBd_movie_sentiment.py
@@ -1,137 +1,136 @@
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader, random_split
 from torchtext.datasets import IMDB
-
-train_dataset = IMDB(split="train")
-test_dataset = IMDB(split="test")
-
-# step1: create the datasets
-from torch.utils.data.dataset import random_split
-
-torch.manual_seed(1)
-train_dataset, valid_dataset = random_split(list(train_dataset), 
-                                        [20000, 5000])
-
-# step2: find unique tokens
 import re
 from collections import Counter, OrderedDict
 
+# Load datasets
+train_dataset = IMDB(split="train")
+test_dataset = IMDB(split="test")
+torch.manual_seed(1)
+train_dataset, valid_dataset = random_split(list(train_dataset), [20000, 5000])
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Tokenizer and vocabulary
 def tokenizer(text):
     text = re.sub('<[^>]*>', '', text)
     emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text.lower())
-    text = re.sub('[\W]+', ' ', text.lower()) \
-    + ' '.join(emoticons).replace('-', '')
-    tokenized = text.split()
-    return tokenized
+    text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', '')
+    return text.split()
 
 token_counts = Counter()
 for label, line in train_dataset:
     tokens = tokenizer(line)
     token_counts.update(tokens)
 
-#print("Vocabulary size:", len(token_counts))
-
-# Step3: encoding each unique token into integers
 from torchtext.vocab import vocab
-sorted_by_frew_tuples = sorted(
-    token_counts.items(), key=lambda x: x[1], reverse=True
-)
-ordered_dict = OrderedDict(sorted_by_frew_tuples)
+sorted_by_freq_tuples = sorted(token_counts.items(), key=lambda x: x[1], reverse=True)
+ordered_dict = OrderedDict(sorted_by_freq_tuples)
 vocab = vocab(ordered_dict)
 vocab.insert_token("<pad>", 0)
 vocab.insert_token("<unk>", 1)
 vocab.set_default_index(1)
 
-#print([vocab[token] for token in ['this', 'is', 'an', 'example']])
-
-# Define the functions for transformation
+# Encoding functions
 text_pipeline = lambda x: [vocab[token] for token in tokenizer(x)]
-label_pipeline = lambda x: 1. if x == 'pos' else 0.
-
-## Wrap the encode and transformation function
+label_pipeline = lambda x: 1.0 if x == 'pos' else 0.0
 
 def collate_batch(batch):
     label_list, text_list, lengths = [], [], []
     for _label, _text in batch:
         label_list.append(label_pipeline(_label))
-        processed_text = torch.tensor(text_pipeline(_text),
-                            dtype=torch.int64)
+        processed_text = torch.tensor(text_pipeline(_text), dtype=torch.int64)
         text_list.append(processed_text)
         lengths.append(processed_text.size(0))
 
-    label_list = torch.tensor(label_list)
+    label_list = torch.tensor(label_list, dtype=torch.float32)
     lengths = torch.tensor(lengths)
-    padded_text_list = nn.utils.rnn.pad_sequence(
-        text_list, batch_first=True
-    )
+    padded_text_list = nn.utils.rnn.pad_sequence(text_list, batch_first=True)
     return padded_text_list, label_list, lengths
 
-# ## Take a s small abtch
-from torch.utils.data import DataLoader
-# dataloader = DataLoader(train_dataset, batch_size=4, shuffle=False,
-#                         collate_fn=collate_batch)
-# text_batch, label_batch, length_batch = next(iter(dataloader))
-#print(text_batch)
-
-## Dataloaders
+# Dataloaders
 batch_size = 32
-train_dl = DataLoader(train_dataset, batch_size=batch_size,
-                     shuffle=True, collate_fn=collate_batch)
-valid_dl = DataLoader(valid_dataset, batch_size=batch_size,
-                     shuffle=True, collate_fn=collate_batch)
-test_dl = DataLoader(test_dataset, batch_size=batch_size,
-                     shuffle=True, collate_fn=collate_batch)
+train_dl = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_batch)
+valid_dl = DataLoader(valid_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_batch)
+test_dl = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_batch)
 
-## Embedding
-embedding = nn.Embedding(
-    num_embeddings=10,
-    embedding_dim=3,
-    padding_idx=0)
-
-
+# RNN Model with Dropout
 class RNN(nn.Module):
-    def __init__(self, vocab_size, embed_dim, rnn_hidden_size,
-               fc_hidden_size):
-        super().__init__()
-        self.rnn = nn.LSTM(embed_dim, rnn_hidden_size,
-                        batch_first=True)
-        self.embedding = nn.Embedding(vocab_size,
-                                    embed_dim,
-                                    padding_idx=0)
+    def __init__(self, vocab_size, embed_dim, rnn_hidden_size, fc_hidden_size, dropout=0.5):
+        super(RNN, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_dim, padding_idx=0)
+        self.rnn = nn.LSTM(embed_dim, rnn_hidden_size, batch_first=True, dropout=dropout)
         self.fc1 = nn.Linear(rnn_hidden_size, fc_hidden_size)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(fc_hidden_size, 1)
         self.sigmoid = nn.Sigmoid()
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, text, lengths):
-        out = self.embedding(text)
-        out = nn.utils.rnn.pack_padded_sequence(
-            out, lengths.cpu().numpy(), enforce_sorted=False, batch_first=True
-        )
-        out, (hidden, cell) = self.rnn(out)
-        out = hidden[-1, :, :]
+        embedded = self.embedding(text)
+        packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded, lengths.cpu(), batch_first=True, enforce_sorted=False)
+        packed_output, (hidden, cell) = self.rnn(packed_embedded)
+        out = self.dropout(hidden[-1])
         out = self.fc1(out)
         out = self.relu(out)
+        out = self.dropout(out)
         out = self.fc2(out)
-        out = self.sigmoid(out)
-        return out
+        return self.sigmoid(out)
 
+# Model Initialization
 vocab_size = len(vocab)
-embed_dim = 20
+embed_dim = 50
 rnn_hidden_size = 64
 fc_hidden_size = 64
-torch.manual_seed(1)
-model = RNN(vocab_size, embed_dim, rnn_hidden_size, fc_hidden_size)
+model = RNN(vocab_size, embed_dim, rnn_hidden_size, fc_hidden_size).to(device)
 
+# Loss, Optimizer, Scheduler
+loss_fn = nn.BCELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.9)
 
-# train
+# Training and Evaluation Functions
 def train(dataloader):
     model.train()
     total_acc, total_loss = 0, 0
+    for text_batch, label_batch, lengths in dataloader:
+        text_batch, label_batch, lengths = text_batch.to(device), label_batch.to(device), lengths.to(device)
+        optimizer.zero_grad()
+        pred = model(text_batch, lengths).squeeze()
+        loss = loss_fn(pred, label_batch)
+        loss.backward()
+        optimizer.step()
 
+        total_acc += ((pred >= 0.5).float() == label_batch).sum().item()
+        total_loss += loss.item() * label_batch.size(0)
 
+    return total_acc / len(dataloader.dataset), total_loss / len(dataloader.dataset)
 
+def evaluate(dataloader):
+    model.eval()
+    total_acc, total_loss = 0, 0
+    with torch.no_grad():
+        for text_batch, label_batch, lengths in dataloader:
+            text_batch, label_batch, lengths = text_batch.to(device), label_batch.to(device), lengths.to(device)
+            pred = model(text_batch, lengths).squeeze()
+            loss = loss_fn(pred, label_batch)
 
+            total_acc += ((pred >= 0.5).float() == label_batch).sum().item()
+            total_loss += loss.item() * label_batch.size(0)
 
+    return total_acc / len(dataloader.dataset), total_loss / len(dataloader.dataset)
 
+# Training Loop
+num_epochs = 10
+for epoch in range(num_epochs):
+    train_acc, train_loss = train(train_dl)
+    valid_acc, valid_loss = evaluate(valid_dl)
+    scheduler.step()
+    print(f"Epoch {epoch+1}/{num_epochs}, Train Acc: {train_acc:.4f}, Valid Acc: {valid_acc:.4f}")
+
+# Testing
+test_acc, _ = evaluate(test_dl)
+print(f"Test Accuracy: {test_acc:.4f}")
 
